07840bee5bc0e3d464d2a62e0bff214f87cde6bd
Ross <axcess1@me.com>
2014-05-14 22:03:26 -0400

    remove dummy

--- spec/dummy/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = 'b28dcc3db4d898f760f69e8e9c7ddcb651deaafca9256997c844591c8a12ae0840056148af03bf434de9a493f9ebc0582e0a2ab28478cedc37e5b7b7d343e875'
--- test/dust_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class DustTest < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, Dust
-  end
-end
