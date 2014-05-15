977e2aba4862b17b2bf5daa98e5ed06bc040a72a
Ross <axcess1@me.com>
2014-05-15 18:37:44 -0400

    fiix weird behavior where first image uploaded in a session was not being processed.

--- app/uploaders/gallery_photo_uploader.rb
+++ app/uploaders/gallery_photo_uploader.rb
@@ -8,19 +8,18 @@ class GalleryPhotoUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  process :full_quality
-
-  def full_quality
-    self.class.process :resize_to_fit => Dust.config.full_image_dimensions
-  end
+  process :dust_resize_to_fit
 
   version :thumb do
-    process :create_thumb
+    process :dust_resize_to_fill
   end
 
-  def create_thumb
-    self.class.process :resize_to_fill => Dust.config.thumb_image_dimensions
+  def dust_resize_to_fit
+    resize_to_fit *(Dust.config.full_image_dimensions)
+  end
+
+  def dust_resize_to_fill
+    resize_to_fill *(Dust.config.thumb_image_dimensions)
   end
 
 end
-
